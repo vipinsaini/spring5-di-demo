e808e8256b370b4c4f1d7b289f9db9a8eda66946
Vipin Saini <sainivipin@johndeere.com>
2018-06-09 09:41:06 -0500

    di-demo

---
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -0,0 +1,14 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!!!!!");
+
+        return "Hello";
+    }
+
+}
