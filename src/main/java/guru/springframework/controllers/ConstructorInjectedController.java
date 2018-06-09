c7c2548555dbc95f7c9892d10d7343474f1cfde1
Vipin Saini <sainivipin@johndeere.com>
2018-06-09 16:25:34 -0500

    di-demo-using-spring

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,20 +1,20 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 5/24/17.
- */
+@Controller
 public class ConstructorInjectedController {
 
+    @Autowired
     private GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public String sayHello(){
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 }
--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -1,15 +1,16 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 5/24/17.
- */
+@Controller
 public class SetterInjectedController {
+
+    @Autowired
     private GreetingService greetingService;
 
-    public String sayHello(){
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 
