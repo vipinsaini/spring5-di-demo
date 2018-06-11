b7f0c0e03269a95f36c9ee6746257f953347dfed
Vipin Saini <sainivipin@johndeere.com>
2018-06-11 17:07:47 -0500

    di-demo-using-spring-profile-annotations-default

--- src/main/java/guru/springframework/controllers/GetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/GetterInjectedController.java
@@ -5,14 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt on 5/24/17.
- */
 @Controller
 public class GetterInjectedController {
     private GreetingService greetingService;
 
-    public String sayHello(){
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 
--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("es")
+@Profile("de")
 @Primary
-public class PrimarySpanishGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Servicio de Saludo Primario";
+        return "Gremen Primary Greeting Service";
     }
 }
